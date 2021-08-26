3443e39d13e2decdf04489785b2ad0e267f40be9
AndreiYantsevich <yantsevich92@gmail.com>
2021-08-26 21:54:19 +0300

    add nav button

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import style from './Header.module.css'
+import {NavLink} from 'react-router-dom'
+import {PATH} from './Routes';
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={style.dropdown}>
+            <SuperButton>X</SuperButton>
+            <div className={style.dropdownContent}>
+                <NavLink to={PATH.PRE_JUNIOR} className={style.link}>PreJunior </NavLink>
+                <NavLink to={PATH.JUNIOR} className={style.link}>Junior </NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS} className={style.link}>JuniorPlus</NavLink>
+            </div>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,9 @@
+import HW12 from "../../h12/HW12";
+
+export function JuniorPlus() {
+    return (
+        <div>
+            <HW12/>
+        </div>
+    )
+}
