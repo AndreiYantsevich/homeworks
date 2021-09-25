ba0431db254e959d3747123569bca95911041333
AndreiYantsevich <yantsevich92@gmail.com>
2021-09-25 20:11:33 +0300

    complete HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,54 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+type SortByName = {
+    type: 'sort'
+    payload: {
+        direction: string
+    }
+}
+
+type SortByAge = {
+    type: 'check'
+    payload: {
+        age: number
+    }
+}
+
+type ActionsType = SortByName | SortByAge
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionsType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            if (action.payload.direction === 'up') {
+                state.sort((a, b) => a.name > b.name ? 1 : -1)
+            }
+            if (action.payload.direction === 'down') {
+                state.sort((a, b) => a.name < b.name ? 1 : -1)
+            }
+            return [...state]
         }
         case 'check': {
-            // need to fix
-            return state
+            return [...state.filter((e) => e.age > action.payload.age)]
         }
-        default: return state
+        default:
+            return state
+    }
+}
+
+export const sortByNameAC = (direction: string): SortByName => {
+    return {
+        type: 'sort',
+        payload: {
+            direction
+        }
+    }
+}
+
+export const sortByAgeAC = (age: number): SortByAge => {
+    return {
+        type: 'check',
+        payload: {
+            age
+        }
     }
 }
