9dc2067205d659a45e09467812bce477d57b27fb
AndreiYantsevich <yantsevich92@gmail.com>
2021-09-25 16:18:40 +0300

    refactor HW3

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -13,11 +13,7 @@ function HW3() {
     const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
 
     const addUserCallback = (name: string) => { // need to fix any
-        const user: UserType  = {
-            _id: v1(),
-            name,
-        }
-        setUsers([...users, user]) // need to fix
+        setUsers([...users, {_id: v1(), name}]) // need to fix
     }
 
     return (
